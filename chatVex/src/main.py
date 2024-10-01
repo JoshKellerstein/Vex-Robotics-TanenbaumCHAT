8b34dc472307eb4a4b1557f70aa3a14a206aeead
mjm1116 <86253733+mjm1116@users.noreply.github.com>
2024-10-01 13:17:30 -0400

    created vex project

---
+++ chatVex/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       mjm16                                                        #
+# 	Created:      10/1/2024, 1:16:03 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
